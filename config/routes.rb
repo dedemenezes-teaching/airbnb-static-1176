1dcbf58bdf6c13bf7f1c51422b7b7fbe023760b2
Andre Menezes <djdedemenezes@gmail.com>
2023-05-12 14:25:43 -0300

    add flats#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb '/path', to: 'controller#action'
+  # get '/', to: 'flats#index', as: :root
+  root "flats#index"
 end
